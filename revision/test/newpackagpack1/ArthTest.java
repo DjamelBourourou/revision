10b859fa5776515a8af62b347984f12bfc675de9
Djamel <djamelbourourou@gmail.com>
2025-06-21 01:31:42 +0200

    premier commit:ajout du projet java web

---
+++ revision/test/newpackagpack1/ArthTest.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package newpackagpack1;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author pc
+ */
+public class ArthTest {
+    
+    public ArthTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of multip method, of class Arth.
+     */
+    @Test
+    public void testMultip() {
+      
+   
+        Arth instance = new Arth();
+       
+        int result = instance.multip(4, 6);
+        assertEquals(24, result);
+     
+    }
+    
+}
