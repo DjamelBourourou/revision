b60b7c6643131e7fc6752befa785e96bc0560105
DjamelBourourou <djamelbourourou1962@gmail.com>
2025-06-21 14:10:10 +0200

    Update ArthTest.javaSuppression des elements inutiles:

--- revision/test/newpackagpack1/ArthTest.java
+++ revision/test/newpackagpack1/ArthTest.java
@@ -21,21 +21,9 @@ public class ArthTest {
     public ArthTest() {
     }
     
-    @BeforeClass
-    public static void setUpClass() {
-    }
+
     
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
-    }
+  
 
     /**
      * Test of multip method, of class Arth.
